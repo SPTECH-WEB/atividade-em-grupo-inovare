d4117c005e3b716eead80e674475446fa3e4d2d1
Lzsaam <samuel.lima@sptech.school>
2025-04-21 17:56:48 -0300

    Adicionando Controller

--- Entregas/src/main/java/com/inovare/entregas/controller/PedidoController.java
+++ Entregas/src/main/java/com/inovare/entregas/controller/PedidoController.java
@@ -1,4 +1,31 @@
 package com.inovare.entregas.controller;
 
-public class PedidoController {
-}
+        import com.inovare.entregas.model.Pedido;
+        import com.inovare.entregas.service.PedidoService;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.*;
+
+        import java.util.Optional;
+
+        @RestController
+        @RequestMapping("/pedidos")
+        public class PedidoController {
+
+            @Autowired
+            private PedidoService pedidoService;
+
+            @GetMapping("/calcular-frete")
+            public double calcularFrete(@RequestParam double peso, @RequestParam String tipoFrete) {
+                return pedidoService.calcularFrete(peso, tipoFrete);
+            }
+
+            @PostMapping
+            public Pedido salvarPedido(@RequestBody Pedido pedido) {
+                return pedidoService.salvarPedido(pedido);
+            }
+
+            @GetMapping("/{id}")
+            public Optional<Pedido> buscarPedidoPorId(@PathVariable Long id) {
+                return pedidoService.buscarPedidoPorId(id);
+            }
+        }
--- Entregas/src/main/java/com/inovare/entregas/service/PedidoService.java
+++ Entregas/src/main/java/com/inovare/entregas/service/PedidoService.java
@@ -1,4 +1,47 @@
 package com.inovare.entregas.service;
 
+import com.inovare.entregas.model.Pedido;
+import com.inovare.entregas.repository.PedidoRepository;
+import com.inovare.entregas.service.strategy.FreteEconomico;
+import com.inovare.entregas.service.strategy.FreteExpresso;
+import com.inovare.entregas.service.strategy.FreteGratis;
+import com.inovare.entregas.service.strategy.FreteStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class PedidoService {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    public Pedido salvarPedido(Pedido pedido) {
+        return pedidoRepository.save(pedido);
+    }
+
+    public Optional<Pedido> buscarPedidoPorId(Long id) {
+        return pedidoRepository.findById(id);
+    }
+
+    public double calcularFrete(double peso, String tipoFrete) {
+        FreteStrategy freteStrategy;
+
+        switch (tipoFrete.toLowerCase()) {
+            case "economico":
+                freteStrategy = new FreteEconomico();
+                break;
+            case "expresso":
+                freteStrategy = new FreteExpresso();
+                break;
+            case "gratis":
+                freteStrategy = new FreteGratis();
+                break;
+            default:
+                throw new IllegalArgumentException("Tipo de frete inválido");
+        }
+
+        return freteStrategy.calcularFrete(peso);
+    }
 }
